157eb1e79668bff6b22b539e4e59ed0d0d112f12
bababa <pijonsan@gmail.com>
2026-05-05 22:13:15 +0900

    fix

--- internal/bpf/generate.go
+++ internal/bpf/generate.go
@@ -1,3 +1,6 @@
-package bpf
-
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target bpfel -type retransmit_event -cc clang -cflags "-O2 -g -Wall -I../../bpf/vmlinux" Retransmit ../../bpf/retransmit.bpf.c
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go \
+//  -target bpfel \
+//  -type retransmit_event \
+//  -cc clang \
+//  -cflags "-O2 -g -Wall -target bpf -D__TARGET_ARCH_x86 -I../../bpf" \
+//  Retransmit ../../bpf/retransmit.bpf.c
