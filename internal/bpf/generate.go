d4d9ada0392f2587a38b603dcd32627e7b1a433e
bababa <pijonsan@gmail.com>
2026-05-05 19:39:52 +0900

    fix

--- internal/bpf/generate.go
+++ internal/bpf/generate.go
@@ -1,6 +1,3 @@
 package bpf
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go \
-//   -cc clang \
-//   -cflags "-O2 -g -Wall -I../../bpf/vmlinux" \
-//   Retransmit ../../bpf/retransmit.bpf.c
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang -cflags "-O2 -g -Wall -I../../bpf/vmlinux" Retransmit ../../bpf/retransmit.bpf.c
